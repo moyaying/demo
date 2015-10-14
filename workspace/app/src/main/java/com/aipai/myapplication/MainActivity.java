ac27ebe947db2d0061bd24415f3103ee5828564a
moo <moyaying88@gmail.com>
2015-10-14 18:00:32 +0800

    git commit 2

--- workspace/app/src/main/java/com/aipai/myapplication/MainActivity.java
+++ workspace/app/src/main/java/com/aipai/myapplication/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String nativRet = NativeBus.getInstance().invoke("MainActivity", "params");
+                String nativRet = NativeBus.getInstance().invoke("MainActivity", "parameters");
                 Toast.makeText(MainActivity.this, nativRet, Toast.LENGTH_SHORT).show();
             }
         });
